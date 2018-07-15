d21e1a9c84b2811afc9a996e53e478ddc1c7f4aa
Belal Khan <probelalkhan@gmail.com>
2018-07-15 20:47:14 +0530

    Reading All Users

--- includes/DbOperations.php
+++ includes/DbOperations.php
@@ -45,6 +45,22 @@
             return $password; 
         }
 
+        public function getAllUsers(){
+            $stmt = $this->con->prepare("SELECT id, email, name, school FROM users;");
+            $stmt->execute(); 
+            $stmt->bind_result($id, $email, $name, $school);
+            $users = array(); 
+            while($stmt->fetch()){ 
+                $user = array(); 
+                $user['id'] = $id; 
+                $user['email']=$email; 
+                $user['name'] = $name; 
+                $user['school'] = $school; 
+                array_push($users, $user);
+            }             
+            return $users; 
+        }
+
         public function getUserByEmail($email){
             $stmt = $this->con->prepare("SELECT id, email, name, school FROM users WHERE email = ?");
             $stmt->bind_param("s", $email);
@@ -54,7 +70,7 @@
             $user = array(); 
             $user['id'] = $id; 
             $user['email']=$email; 
-            $user['password'] = $name; 
+            $user['name'] = $name; 
             $user['school'] = $school; 
             return $user; 
         }
--- public/index.php
+++ public/index.php
@@ -135,6 +135,25 @@ $app->post('/userlogin', function(Request $request, Response $response){
         ->withStatus(422);    
 });
 
+$app->get('/allusers', function(Request $request, Response $response){
+
+    $db = new DbOperations; 
+
+    $users = $db->getAllUsers();
+
+    $response_data = array();
+
+    $response_data['error'] = false; 
+    $response_data['users'] = $users; 
+
+    $response->write(json_encode($response_data));
+
+    return $response
+    ->withHeader('Content-type', 'application/json')
+    ->withStatus(200);  
+
+});
+
 function haveEmptyParameters($required_params, $response){
     $error = false; 
     $error_params = '';
